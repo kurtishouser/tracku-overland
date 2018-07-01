180caabff72f86160beee176202fccaf597c6fa3
Kurtis Houser <houserkurtis@gmail.com>
2018-07-01 10:41:52 -0700

    Intial commit - Setup server scaffolding

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const routes = require('./server/routes');
+
+app.use('/', routes);
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  /* eslint-disable no-console */
+  console.log(`Express server listening on port ${PORT}`);
+});
