a115429ed6c51bac21c1f219aa6416ef6d9d13ba
Kurtis Houser <houserkurtis@gmail.com>
2018-07-07 10:00:57 -0700

    Improve checks and error messages in authenticateDevice middleware

--- server/middleware/authenticateDevice.js
+++ server/middleware/authenticateDevice.js
@@ -1,6 +1,15 @@
 module.exports = (req, res, next) => {
+  if (!process.env.DEVICE_AUTH_TOKEN) {
+    return next();
+  }
+
+  if (!req.query.token) {
+    return res.status(401).json({error: 'no token provided'})
+  }
+
   if (req.query.token === process.env.DEVICE_AUTH_TOKEN) {
     return next();
   }
-  res.sendStatus(401);
+
+  res.status(401).json({error: 'invalid token'});
 }
