8a97887912ad287375ac12e9a067a71e95b72d58
Kurtis Houser <houserkurtis@gmail.com>
2019-11-14 19:11:53 -0800

    Fix location schema (trip complete section)

--- server/models/location.js
+++ server/models/location.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const locationSchema = mongoose.Schema({
-  type: String,
+  type: {type: String},
   geometry: {
     type: {type: String},
     coordinates: [Number],
@@ -19,19 +19,62 @@ const locationSchema = mongoose.Schema({
     altitude: Number,
     battery_state: String,
 
-    // trip (present when using trip functionality in iOS app)
+    // trip complete (when using trip functionality in iOS app)
     start: Date,
-    "end-coordinates": [Number],
+    end_location: {
+      type: {type: String},
+      geometry: {
+        type: {type: String},
+        coordinates: [Number],
+      },
+      properties: {
+        motion: [String],
+        speed: Number,
+        battery_level: Number,
+        wifi: String,
+        device_id: String,
+        vertical_accuracy: Number,
+        horizontal_accuracy: Number,
+        timestamp: Date,
+        altitude: Number,
+        battery_state: String,
+      },
+    },
     type: {type: String},
     stopped_automatically: Boolean,
     mode: String,
-    "start-coordinates": [Number],
+    start_location: {
+      type: {type: String},
+      geometry: {
+        type: {type: String},
+        coordinates: [Number],
+      },
+      properties: {
+        motion: [String],
+        speed: Number,
+        battery_level: Number,
+        wifi: String,
+        device_id: String,
+        vertical_accuracy: Number,
+        horizontal_accuracy: Number,
+        timestamp: Date,
+        altitude: Number,
+        battery_state: String,
+        // optional tracking stats (when enabled in iOS app)
+        pauses: Boolean,
+        deferred: Number,
+        significant_change: Number,
+        locations_in_payload: Number,
+        activity: String,
+        desired_accuracy: Number,
+      },
+    },
     end: Date,
     duration: Number,
     distance: Number,
     steps: Number,
 
-    // tracking stats (present when option enabled in iOS app)
+    // optional tracking stats (when enabled in iOS app)
     pauses: Boolean,
     deferred: Number,
     significant_change: Number,
