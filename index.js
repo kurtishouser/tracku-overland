b4c740aedfa9b32c7fcbad9b644ff4e1c1e8d65e
Kurtis Houser <houserkurtis@gmail.com>
2019-11-15 09:07:52 -0800

    Move server setup to db connection callbabck

--- index.js
+++ index.js
@@ -16,23 +16,25 @@ mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB
 // mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 
 mongoose.connection
-    .on('error', error => console.log('Error connecting to MongoDB:', error))
-    .once('open', () => console.log('Connected to MongoDB.'))
+  .on('error', error => console.log('Error connecting to MongoDB:', error))
+  .once('open', () => {
+    console.log('Connected to MongoDB.');
 
-const app = express();
+    const app = express();
 
-app.use(bodyParser.json({limit: '16mb'})); // increased to accommodate 1000 points/batch
-app.use(cors());
+    app.use(bodyParser.json({limit: '16mb'})); // increased to accommodate 1000 points/batch
+    app.use(cors());
 
-app.use('/', routes);
+    app.use('/', routes);
 
-app.use((req, res) => {
-  res.sendStatus(404);
-});
+    app.use((req, res) => {
+      res.sendStatus(404);
+    });
 
-const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  /* eslint-disable no-console */
-  console.log(`Express server listening on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      /* eslint-disable no-console */
+      console.log(`Express server listening on port ${PORT}`);
+    });
+  });
