d9937b4e5eb62266ae8a763d9087fb225da339ae
Kurtis Houser <houserkurtis@gmail.com>
2019-11-22 10:38:54 -0800

    Update (add defaults) and reorder Location schema

--- server/models/location.js
+++ server/models/location.js
@@ -20,37 +20,24 @@ const locationSchema = new mongoose.Schema({
     battery_state: String,
 
     // trip complete (when using trip functionality in iOS app)
+    // the defaults for coordinates and motion prevent persisting
+    // empty data when there is no 'trip complete' data
+    type: {type: String}, // 'trip'
     start: Date,
-    end_location: {
-      type: {type: String},
-      geometry: {
-        type: {type: String},
-        coordinates: [Number],
-      },
-      properties: {
-        motion: [String],
-        speed: Number,
-        battery_level: Number,
-        wifi: String,
-        device_id: String,
-        vertical_accuracy: Number,
-        horizontal_accuracy: Number,
-        timestamp: Date,
-        altitude: Number,
-        battery_state: String,
-      },
-    },
-    type: {type: String},
+    end: Date,
+    duration: Number,
+    distance: Number,
+    steps: Number,
     stopped_automatically: Boolean,
     mode: String,
     start_location: {
       type: {type: String},
       geometry: {
         type: {type: String},
-        coordinates: [Number],
+        coordinates: {type: [Number], default: undefined},
       },
       properties: {
-        motion: [String],
+        motion: {type: [String], default: undefined},
         speed: Number,
         battery_level: Number,
         wifi: String,
@@ -69,10 +56,25 @@ const locationSchema = new mongoose.Schema({
         desired_accuracy: Number,
       },
     },
-    end: Date,
-    duration: Number,
-    distance: Number,
-    steps: Number,
+    end_location: {
+      type: {type: String},
+      geometry: {
+        type: {type: String},
+        coordinates: {type: [Number], default: undefined},
+      },
+      properties: {
+        motion: {type: [String], default: undefined},
+        speed: Number,
+        battery_level: Number,
+        wifi: String,
+        device_id: String,
+        vertical_accuracy: Number,
+        horizontal_accuracy: Number,
+        timestamp: Date,
+        altitude: Number,
+        battery_state: String,
+      },
+    },
 
     // optional tracking stats (when enabled in iOS app)
     pauses: Boolean,
