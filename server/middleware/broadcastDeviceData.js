e43ef4f934b0c43cf3a7fec4de8dace48e384112
Kurtis Houser <houserkurtis@gmail.com>
2019-11-26 22:31:02 -0800

    Add check for current property in device data

--- server/middleware/broadcastDeviceData.js
+++ server/middleware/broadcastDeviceData.js
@@ -1,7 +1,9 @@
 const { socketIO } = require('../config/socket');
 
 module.exports = (req, res, next) => {
-  if (req.body.locations) {
+  if (req.body.current) {
+    socketIO('/tracker').emit('location-updated', req.body.current);
+  } else if (req.body.locations) {
     socketIO('/tracker').emit('location-updated', req.body.locations[req.body.locations.length - 1]);
   }
 
