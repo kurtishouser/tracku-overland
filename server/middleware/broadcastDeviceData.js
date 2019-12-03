959d89ee1a174100d7afd6f1c0556d31feca67d9
Kurtis Houser <houserkurtis@gmail.com>
2019-12-03 13:10:28 -0800

    Fix socket.io emit logic

--- server/middleware/broadcastDeviceData.js
+++ server/middleware/broadcastDeviceData.js
@@ -1,14 +1,31 @@
 const { socketIO } = require('../config/socket');
 
 module.exports = (req, res, next) => {
-  if (req.body.current) {
-    socketIO('/tracker').emit('location-updated', req.body.current);
-  } else if (req.body.locations) {
-    socketIO('/tracker').emit('location-updated', req.body.locations[req.body.locations.length - 1]);
+  const { locations, current, trip } = req.body;
+  let currentLocation;
+
+  if (locations) {
+    currentLocation = locations[req.body.locations.length - 1];
+
+    const tripComplete = locations.filter(location => location.properties.type === 'trip');
+    if (tripComplete.length > 0) {
+      socketIO('/tracker').emit('trip-complete', tripComplete);
+    }
   }
 
-  if (req.body.trip) {
-    socketIO('/tracker').emit('trip-active', req.body.trip);
+  if (current) {
+    currentLocation = current;
+  }
+
+  if (currentLocation) {
+    socketIO('/tracker').emit('location-updated', currentLocation);
+  }
+
+  // start_location check is a rare edge case (Overland 1.2 iOS bug?)
+  // if queued data is sent immediately after a trip is started the start_location object may be null
+  // taking care of it here so client apps won't need check for it
+  if (trip && trip.start_location) {
+    socketIO('/tracker').emit('trip-active', trip);
   }
 
   return next();
