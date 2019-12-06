7e88223e6a2692736024088bda3f633661aa9855
Kurtis Houser <houserkurtis@gmail.com>
2019-12-05 22:23:26 -0800

    Combine all incoming device data into one emit event

--- server/middleware/broadcastDeviceData.js
+++ server/middleware/broadcastDeviceData.js
@@ -2,30 +2,21 @@ const { socketIO } = require('../config/socket');
 
 module.exports = (req, res, next) => {
   const { locations, current, trip } = req.body;
-  let currentLocation;
 
-  if (locations) {
-    currentLocation = locations[req.body.locations.length - 1];
-
-    const tripComplete = locations.filter(location => location.properties.type === 'trip');
-    if (tripComplete.length > 0) {
-      socketIO('/tracker').emit('trip-complete', tripComplete);
-    }
-  }
-
-  if (current) {
-    currentLocation = current;
-  }
-
-  if (currentLocation) {
-    socketIO('/tracker').emit('location-updated', currentLocation);
-  }
-
-  // start_location check is a rare edge case (Overland 1.2 iOS bug?)
-  // if queued data is sent immediately after a trip is started the start_location object may be null
-  // taking care of it here so client apps won't need check for it
-  if (trip && trip.start_location) {
-    socketIO('/tracker').emit('trip-active', trip);
+  // trip.start_location check below is a rare edge case (Overland 1.2 iOS bug?)
+  // If queued data is sent from the device immediately after a trip is started
+  // the start_location object may be null.
+  // Ignoring it here so client apps won't need to check for it.
+  if (locations) { // probably unnecessary but just in case
+    socketIO('/tracker').emit('device-update', {
+      currentLocation: current
+        ? current
+        : locations[req.body.locations.length - 1],
+      currentTrip: trip && trip.start_location ? trip : null,
+      completedTrips: locations.filter(
+        location => location.properties.type === 'trip'
+      ),
+    });
   }
 
   return next();
