e982c222c8f541808a66442120a07e0927cfa2a3
Kurtis Houser <houserkurtis@gmail.com>
2022-03-07 15:36:40 -0600

    Update & freeze dependencies, update time.js

--- server/utils/time.js
+++ server/utils/time.js
@@ -1,10 +1,10 @@
-const geoTz = require('geo-tz');
+const { find } = require('geo-tz');
 const moment = require('moment-timezone');
 
 // accepts a location object
 const addLocalTimeProperties = ({ geometry, properties }) => {
   // IANA time zone
-  const localTimeZone = geoTz(
+  const localTimeZone = find(
     geometry.coordinates[1],
     geometry.coordinates[0]
   )[0]; // can be multiple, only use the first element for now
